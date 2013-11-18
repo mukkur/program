dfbc538977f37bf3e65925687be3ab8fda25bb6e
mukkur <rajasekharm4u@gmail.com>
2013-11-18 17:46:15 -0500

    so

---
+++ BasicContacts/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public static List<string> Data = new List<string> { "Value1", "Value2" };
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return Data[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            Data.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            Data[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Data.RemoveAt(id);
+        }
+    }
+}
