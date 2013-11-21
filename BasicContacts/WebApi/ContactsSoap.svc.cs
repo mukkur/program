6b9257e880ac0059d8a6cef34e72fbac2f18911d
mukkur <rajasekharm4u@gmail.com>
2013-11-21 17:51:03 -0500

    wheel

---
+++ BasicContacts/WebApi/ContactsSoap.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ContactsSoap" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ContactsSoap.svc or ContactsSoap.svc.cs at the Solution Explorer and start debugging.
+    public class ContactsSoap : IContactsSoap
+    {
+        public String DoWork()
+        {
+            return "Hello world";
+        }
+        public IEnumerable<Contact> GetContacts()
+        {
+            var db = new CSharpContext();
+            return db.Contacts.ToList();
+        }
+    }
+}
---
+++ BasicContacts/WebApi/IContactsSoap.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IContactsSoap" in both code and config file together.
+    [ServiceContract]
+    public interface IContactsSoap
+    {
+        [OperationContract]
+       String DoWork();
+        IEnumerable<Contact> GetContacts();
+    }
+}
