9858c4a665d3002eb826c743786f202a1b852beb
mukkur <rajasekharm4u@gmail.com>
2013-12-16 19:07:06 -0500

    Final

---
+++ WpfFinal/WpfFinal/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WpfFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+             //actb.Filter = Filter;
+    }
+ 
+    private bool Filter(object obj, string text)
+    {
+        XmlElement element = (XmlElement)obj;
+        string CityName = 
+		element.SelectSingleNode("City_name").InnerText.ToLower();
+        if (CityName.Contains(text.ToLower())) return true;
+        return false;
+    }
+ 
+    private void XmlDataProvider_DataChanged(object sender, EventArgs e)
+    {
+        //StatusLabel.Text = "Xml Data Loaded.";
+    }
+        }
+    }
+
+
